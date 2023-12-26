303f6aed76634c4601940181bfd7d87b254431b8
ktruedat <crinnic77@gmail.com>
2023-12-26 14:31:24 +0200

    new decoder

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	tcpOpts := p2p.TCPTransportOpts{
 		ListenAddr:    ":3000",
 		HandshakeFunc: p2p.NOPHandshakeFunc,
-		Decoder:       p2p.GOBDecoder{},
+		Decoder:       p2p.DefaultDecoder{},
 	}
 	transport := p2p.NewTCPTransport(tcpOpts)
 	if err := transport.ListenAndAccept(); err != nil {
